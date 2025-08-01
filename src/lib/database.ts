da084cd6d7e99c46a223f82c885f7afb6843aced
rajyadav63 <vector36razz@gmail.com>
2025-08-01 14:12:58 +0530

    database exported

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -6,7 +6,7 @@ type ConnectionObject = {
 
 const connection: ConnectionObject = {}
 
-async function dbConnect(): Promise<void> {
+export async function dbConnect(): Promise<void> {
     if (connection.isConnected) {
         console.log("Database is already connected");
         return
