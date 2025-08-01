4964ebc4e9ddcc28ecea5c29aa207d6ed3a52eba
rajyadav63 <vector36razz@gmail.com>
2025-08-01 10:07:45 +0530

    signup validate using zod

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,30 @@
+import { email, z } from "zod";
+
+//validate username
+export const usernameValidation = z
+    .string()
+    .min(4, { message: "Username must be at least 4 characters" })
+    .max(20, { message: "Username must be less than 20 characters" })
+    .regex(/^[a-zA-Z0-9]+$/, { message: "Username must contain only letters and numbers" })
+    .trim()
+    .toLowerCase()
+
+//validate email
+export const emailValidation = z
+    .string()
+    .min(4, { message: "Username must be at least 4 characters" })
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Use valid email address")
+
+//validate password
+export const passwordValidation = z
+    .string()
+    .min(4, { message: "Username must be at least 4 characters" })
+    .max(12, { message: "Username must be less than 12 characters" })
+    .trim()
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: emailValidation,
+    password: passwordValidation
+})
+
