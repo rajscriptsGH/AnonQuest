3d5001461e78f76477f0102c7b510dca0cde1585
rajyadav63 <vector36razz@gmail.com>
2025-08-03 19:58:47 +0530

    jwt custom type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,10 +1,23 @@
-import 'next-auth'
+import { DefaultSession } from "next-auth"
 
-declare module 'next-auth' {
-    interface User {
-        _id: string,
-        isVerified?: boolean,
-        isAcceptingMessage?: boolean,
+declare module "next-auth" {
+    interface User extends DefaultSession["user"] {
+        _id: string
+        isVerified?: boolean
+        isAcceptingMessage?: boolean
+        username?: string
+    }
+
+    interface Session {
+        user: User
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        _id: string
+        isVerified?: boolean
+        isAcceptingMessage?: boolean
         username?: string
     }
 }
